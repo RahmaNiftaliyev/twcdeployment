af53557592b8534bb7b9e32a4fee7c011347f643
mahammadSarkarli <mahammad.sarkarli.de@gmail.com>
2022-02-22 14:45:29 +0100

    used react-helmet for meta tags

--- src/features/root/Application.jsx
+++ src/features/root/Application.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-
+import { Helmet } from 'react-helmet';
 class Application extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    // <meta name="title" content="TWC - Peşəkar biznes xidmətləri">
+    // <meta name="description" content="Azərbaycanda ilk yerli saf menecment konsaltinq servisləri">
+    return (
+      <div>
+        <Helmet>
+          <title>TWC - Peşəkar biznes xidmətləri</title>
+          <meta name="description" content="Azərbaycanda ilk yerli saf menecment konsaltinq servisləri" />
+          <meta charset="utf-8" />
+          <link rel="icon" href="%PUBLIC_URL%/navigation-white-logo.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" href="%PUBLIC_URL%/navigation-white-logo.svg" />
+          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+          <meta
+            name="keywords"
+            content="consulting,management,management consulting,azure,baku,azerbaijan,business,chingiz novruzzade,Netflix bussiness model,startap,success"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="revisit-after" content="1 days" />
+          <meta name="author" content="devs:Rahman Niftaliyev | Məhəmməd Sərkərli designer:Sabina Mammadova" />
+        </Helmet>
+      </div>
+    );
+  }
 }
 
 export default Application;
