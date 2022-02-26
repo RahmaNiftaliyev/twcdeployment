e00be6feca1c47b2efcfac2773249b842c74de3a
mahammadSarkarli <mahammad.sarkarli.de@gmail.com>
2022-02-26 10:05:57 +0100

    solved problems in industries

--- src/features/components/industry/IndustryEcommerce.jsx
+++ src/features/components/industry/IndustryEcommerce.jsx
@@ -8,84 +8,65 @@ import headerImagePng from './assets/img/eticaret-header.png';
 import sectionImagePng from './assets/img/eticaret-section.png';
 
 const IndustryEcommerce = () => {
-    return (
-        <div>
-            <IndustryHeaderDash
-                headerText={'SƏNAYE/E-TİCARƏT'}
-                isHeader={true}
-            />
-            <div className={`${styles.container}`}>
-                <section className={`${styles.header_image_container} d-flex `}>
-                    <img src={headerImagePng} alt="" />
-                </section>
-                <section className={`d-flex direction-column`}>
-                    <span className={`${styles.header_subtitle}`}>Sənaye</span>
-                    <span className={`${styles.header_title}`}>Enerji</span>
-                </section>
-                <section className={`${styles.paragraph_section}`}>
-                    <p>
-                        Enerji sektorunun əksəriyyəti neft və ya qaz
-                        ehtiyyatlarının kəşfiyyatı ilə, vəyaxud neft və qazın
-                        qazılması və emalı ilə məşğul olan şirkətlərdən
-                        ibarətdir. Enerji sektoru çox geniş bir termindir və
-                        enerji istehsalı və paylanması ilə məşğul olan
-                        şirkətlərin mürəkkəb və bir-biri ilə əlaqəli şəbəkəsini
-                        təsvir edir. Enerji iqtisadiyyatın gücləndirilməsində,
-                        istehsal və nəqliyyat vasitələrinin asanlaşdırılmasında
-                        mühüm rol oynayır. Enerji sənayesindəki şirkətlər
-                        enerjinin necə qaynaqlandığına görə iki qrupa bölünür;
-                        bərpa olunan (renewable) və bərpa olunmayan
-                        (non-renewable). <br />
-                        Neft və Qaz, Qazma və Hasilat, Boru Kəməri və Emalı,
-                        Mədən Şirkətləri, Bərpa Olunan Enerji (Renewable Energy)
-                        enerji sektoru şirkətlərinin növləridir. Onların hər
-                        biri müəssisə və istehlakçıların enerji ilə təmin
-                        olunmasında müəyyən rol oynayır.
-                    </p>
-                </section>
-                <section className={`${styles.section_image_container} d-flex`}>
-                    <img src={sectionImagePng} alt="" />
-                </section>
+  return (
+    <div>
+      <IndustryHeaderDash headerText={'SƏNAYE/E-TİCARƏT'} isHeader={true} />
+      <div className={`${styles.container}`}>
+        <section className={`${styles.header_image_container} d-flex `}>
+          <img src={headerImagePng} alt="" />
+        </section>
+        <section className={`d-flex direction-column`}>
+          <span className={`${styles.header_subtitle}`}>Sənaye</span>
+          <span className={`${styles.header_title}`}>E-ticarət</span>
+        </section>
+        <section className={`${styles.paragraph_section}`}>
+          <p>
+            E-ticarət, İnternet-ticarət kimi adlanan elektron kommersiya, şirkətlərə oz məhsul və xidmətlərini online
+            üzərindən ticarət və ödənişlərini tamamlamaq üçün tranzaksiya əməliyyatlarını icra edən online sistemdir.
+            Əsas dörd elektron ticarət modeli istehlakçılar və şirkətlər arasında əməliyyatların icrasına imkan yaradır.
+          </p>
+          <p>
+            Bunlar B2B , B2C, C2C, C2B e-ticari əlaqələrdir ki, müştəri və bizneslər öz məhsul və xidmətlərini
+            alqı-satqısını reallaşdırır. E-ticarətin fərqli formatda fəaliyyəti və tranzaksiyaların icrası istehlakçılar
+            və istehsalçılar arasında təmin edir. Bu istiqamətdə TWC komandası olaraq sizin şirkətinizin harada olduğunu
+            və onun e-ticarət strategiyasını ən yaxşı şəkildə analiz etmə imkanlarına malikik:
+          </p>
 
-                <section className={`${styles.paragraph_section}`}>
-                    <p>
-                        Enerji konsaltinqi biznesin enerji istifadəsinin
-                        optimallaşdırmağına yönəlmiş ekoloji konsaltinqin alt
-                        intizamıdır. Enerji konsaltinqi çox vaxt əməliyyat
-                        xərclərinin azaldılmasına yönəlir, baxmayaraq ki, bu
-                        həmişə əsas məqsəd deyil. Geniş ictimaiyyətin şüurunda
-                        korporativ sosial məsuliyyətin (KSM) artan əhəmiyyəti
-                        ilə əlaqədar korporativ səviyyəli müəssisələr öz enerji
-                        balansına daha ekoloji cəhətdən təmiz enerji mənbələrini
-                        daxil etmək üçün enerji məsləhətçisi ilə müqavilə
-                        bağlamağı üstün tuturlar.
-                    </p>
-                    <p>
-                        İstər böyük, istərsə də kiçik bir kommersiya müəssisəsi
-                        olmağınızdan asılı olmayaraq, enerji istehlakınıza və
-                        mənbələrə nəzarət etmək uzunmüddətli dəstək tələb edən
-                        güclü bir addımdır. Optimal məsrəflərin idarə edilməsini
-                        təmin etmək üçün enerji bazarlarının dəyişən
-                        dinamikasını dəvamlı olaraq izah edə biləcək ekspert
-                        lazımdır.
-                    </p>
-                    <p>
-                        Niyə enerji konsultantına müraciət etməliyəm? Bir çox
-                        müəssisələr enerji təminatçısı axtararkən özlərinə bu
-                        sualı verirlər. Energetikanın derequlyasiya edilməsi
-                        seçimi, qənaəti və çevikliyinə əsaslanan bazar yaradıb.
-                        “Thinkwise Business Consulting” kimi müstəqil, qərəzsiz
-                        enerji məsləhətçisi ilə işləmək sizin üçün doğru seçim
-                        ola bilər. “Thinkwise Business Consulting”-dəki
-                        öhdəliyimizin bir hissəsi olaraq, müqavilələriniz
-                        qüvvədə olandan əvvəl, müddətində və sonra sizinlə
-                        işləyirik.
-                    </p>
-                </section>
-            </div>
-            <Footer />
-        </div>
-    );
+          <p className={`${styles.paragraph_title}`}>Təcrübənin dəyərləndirilməsi</p>
+          <p>
+            Avtomatizasiya üzərindən (saytın dinamikası, standartlara uyğunluğu, veb-saytın funksionallığı və onlayn
+            dəstək kimi sahələri əhatə edir) istifadə edərək, e-ticarət üzrə ekspert komandamız rəqamsal təcrübəni
+            qiymətləndirirlər. Hesabları sənayelərarası verilənlər bazası ilə müqayisə edərək, biz güclü tərəfləri, zəif
+            tərəfləri və diqqət edilməli istiqamətləri müəyyən edirik.
+          </p>
+
+          <p className={`${styles.paragraph_title}`}>Dizayn dəyərləndirməsi</p>
+          <p>
+            Bizim təqdim edəcəyimiz rəqəmsal təcrübə müştərinin nöqteyi-nəzərindən və onların gözləntilərini nəzərə
+            alaraq araşdırılır. Sosial, etnoqrafik tədqiqat bizə müştərilərin e-ticarət kanalları ilə necə əlaqə
+            saxladığını analiz etməyə imkan verir. Bu kanalların müştəri ehtiyaclarını nə qədər müsbət və ya mənfi
+            şəkildə formalaşdırdığını, müştərilərə səhvlərin qarşısını almağa və onları bərpa etməyə kömək edir.
+          </p>
+
+          <p className={`${styles.paragraph_title}`}>Texniki dəyərləndirmə</p>
+          <p>
+            Bu diaqnostika cari online platformaları, performansı və inteqrasiyaları analiz etməyə və onların e-ticarət
+            strategiyasını necə sürətləndirəcəyini və ona mane olacağını anlamağa kömək edir. Bu, texniki boşluqları
+            analiz edərək və eləcə də onları bərpa edə biləcək yatırımları müəyyən etməyə imkan verir.
+          </p>
+
+          <p className={`${styles.paragraph_title}`}>Bacarıqların dəyərləndirilməsi</p>
+          <p>
+            Biz e-ticarət imkanlarını ölçmək üçün 100-dən çox ölçmə metodlarını əhatə edən və şirkətlərin sənayəsi
+            müqayisə edərək hərtərəfli rəqamsal alətlərdən istifadə edirik. Bu, şirkətlərin korporativ imkanları
+            baxımından hansı seqmentdə daha yaxşı ola biləcəyini və onların harada güclü olduqlarını prioritetləşməyə
+            imkan verir.
+          </p>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default IndustryEcommerce;
